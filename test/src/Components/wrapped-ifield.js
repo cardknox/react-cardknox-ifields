f53c4dabfb337a87829535deef4b7bd6604725cb
Moshe Stein <47797106+mstein-ck@users.noreply.github.com>
2025-04-24 12:41:13 -0400

    Update wrapped-ifield.js

--- test/src/Components/wrapped-ifield.js
+++ test/src/Components/wrapped-ifield.js
@@ -68,7 +68,7 @@ const WrappedIfield = React.forwardRef((props, ref) => {
     type={ifieldType}
     account={account}
     options={options}
-    threeDS={ifieldType === CARD_TYPE ? threeds : null}
+    threeds={ifieldType === CARD_TYPE ? threeds : null}
     onLoad={onLoad}
     onUpdate={onUpdate}
     onSubmit={onSubmit}
